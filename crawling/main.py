db4e4824d3c089afc532ef3aa8aac953ac68c566
9gi_coach21 <9gi_coach21@ssafy.com>
2024-03-06 17:59:20 +0900

    setting : maria db 테스트 진행 중에 있었음

--- crawling/main.py
+++ crawling/main.py
@@ -3,10 +3,12 @@ from fastapi import FastAPI
 from sqlalchemy.orm import Session
 
 from config.database import SessionLocal, engine, Base
+from controllers import users
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+app.include_router(users.router)
 
 def get_db():
     db = SessionLocal()
