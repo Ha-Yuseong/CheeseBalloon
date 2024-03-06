123311668d4c615dccaffcadd0aa6728ab58a736
9gi_coach21 <9gi_coach21@ssafy.com>
2024-03-06 11:42:27 +0900

    setting : maria db 연동하기

--- crawling/main.py
+++ crawling/main.py
@@ -1,8 +1,20 @@
 
 from fastapi import FastAPI
+from sqlalchemy.orm import Session
+
+from config.database import SessionLocal, engine, Base
+
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
 @app.get("/")
 async def base_get_route():
-    return {"message": "hello world"}
+    return {"message": "hello dddworld"}
