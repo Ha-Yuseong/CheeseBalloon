0f6de4b2253cd6e5f6d483589e6ae86c9014ebb3
Suou <baewc95@gmail.com>
2024-06-17 23:00:43 +0900

    fix: 카테고리 이름 없는 경우 수정

--- frontend/cheeseballoon/src/containers/detail/DetailCategoryChart.tsx
+++ frontend/cheeseballoon/src/containers/detail/DetailCategoryChart.tsx
@@ -67,12 +67,16 @@ export default function DetailCategoryChart() {
       const { seriesData, labelsData } = categoriesData.reduce(
         (acc: Accumulator, item: DailyCategoryType) => {
           acc.seriesData.push(item.time);
-          acc.labelsData.push(item.category);
+          if (item.category === "") {
+            acc.labelsData.push("카테고리 없음");
+          } else {
+            acc.labelsData.push(item.category);
+          }
           return acc;
         },
         { seriesData: [], labelsData: [] }
       );
-
+      console.log(categoriesData);
       setSeries(seriesData);
       setLabels(labelsData);
 
@@ -156,7 +160,9 @@ export default function DetailCategoryChart() {
           lists.map((item, idx: number) => (
             <div className={style.list} key={idx}>
               <div className={style.rank}>{idx + 1}</div>
-              <div className={style.name}>{item.title}</div>
+              <div className={style.name}>
+                {item.title === "" ? "카테고리 없음" : item.title}
+              </div>
               <div className={style.time}>
                 <div>
                   {(Math.floor((item.time / 3600) * 10) / 10).toLocaleString()}
